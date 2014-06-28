e94610330ffd734d6cbf5febcf78c73e6696e80d
Carlos Rosquillas <crosquillas@gmail.com>
2014-06-28 14:24:26 -0700

    Add default methods for plugin

--- www/open.js
+++ www/open.js
@@ -1,5 +1,19 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "open", "coolMethod", [arg0]);
+exports.open = function(args, success, error) {
+  var argsArray = 
+    (Object.prototype.toString.call(args) === '[object Array]') ? args : [args];
+
+  function defaultSuccess() {
+    if (typeof success === 'function') {
+      success();
+    }
+  }
+
+  function defaultError() {
+    if (typeof error === 'function') {
+      error();
+    }
+  }
+  exec(defaultSuccess, defaultError, "open", "open", [argsArray]); 
 };
