585aae254f9e4b68e06399347c08aec863abacb6
Carlos Rosquillas <crosquillas@gmail.com>
2014-06-30 14:15:30 -0700

    Add echo code for iOS

--- src/ios/Open.m
+++ src/ios/Open.m
@@ -1,23 +1,23 @@
-/********* CDVOpen.m Cordova Plugin Implementation *******/
+/********* Open.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
 
-@interface CDVOpen : CDVPlugin {
+@interface Open : CDVPlugin {
   // Member variables go here.
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)open:(CDVInvokedUrlCommand*)command;
 @end
 
-@implementation CDVOpen
+@implementation Open
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+- (void)open:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    NSString* path = [command.arguments objectAtIndex:0];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    if (path != nil && [path length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:path];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
