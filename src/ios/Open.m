1229b7f12b6464b9824518d3af2949f799ea77ac
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-01 19:15:42 -0700

    Open url in quicklook

--- src/ios/Open.m
+++ src/ios/Open.m
@@ -4,16 +4,55 @@
 
 - (void)open:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
+    CDVPluginResult* commandResult = nil;
+
     NSString* path = [command.arguments objectAtIndex:0];
 
     if (path != nil && [path length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:path];
+
+        NSURL* url = [NSURL URLWithString:path];
+
+        if (url.isFileURL) {
+
+            QLPreviewController* previewCtrl = [[QLPreviewController alloc] init];
+            previewCtrl.delegate = self;
+            previewCtrl.dataSource = self;
+
+            UIViewController* context = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+            [context presentViewController:previewCtrl animated:YES completion:nil];
+
+            NSLog(@"cordova.bridge.open - Success!");
+            commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+
+        } else {
+            NSLog(@"cordova.bridge.open - Invalid file URL");
+            commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
     } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        NSLog(@"cordova.bridge.open - Missing URL argument");
+        commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+}
+
+#pragma - QLPreviewControllerDataSource Protocol
+
+- (NSInteger) numberOfPreviewItemsInPreviewController:(QLPreviewController *)controller
+{
+    return 1;
+}
+
+- (id <QLPreviewItem>) previewController:(QLPreviewController *)controller previewItemAtIndex:(NSInteger)index
+{
+    return self;
+}
+
+#pragma - QLPreviewItem Protocol
+
+- (NSURL*)previewItemURL
+{
+    return self.url;
 }
 
 @end
