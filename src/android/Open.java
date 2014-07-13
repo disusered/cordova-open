7991ad2fe9775dc8921e44376c8aadec2c59b1b5
Carlos Rosquillas <crosquillas@gmail.com>
2014-07-13 11:13:39 -0700

    Update header and license information

--- src/android/Open.java
+++ src/android/Open.java
@@ -1,6 +1,15 @@
 /**
- * @author Carlos Antonio
-*/
+ * Open.java
+ *
+ * Copyright (C) 2014 Carlos Antonio
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ */
+
 package com.bridge;
 
 import org.apache.cordova.CordovaPlugin;
