a8befed4c94cea741b68d3d7ea78d5a8151c1607
Carlos Rosquillas <crosquillas@gmail.com>
2015-06-12 14:01:49 -0500

    Fix mixed licensing issue

--- src/android/Open.java
+++ src/android/Open.java
@@ -1,15 +1,3 @@
-/**
- * Open.java
- *
- * Copyright (C) 2015 Carlos Antonio
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- */
-
 package com.disusered;
 
 import org.apache.cordova.CordovaPlugin;
